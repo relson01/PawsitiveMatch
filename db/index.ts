38a09d8da0ff1e65c1aa78cf2f46dd12067f7d1e
relson01 <relsondsouza01@gmail.com>
2023-12-10 11:31:14 +0530

    basic setup

---
+++ db/index.ts
@@ -0,0 +1,20 @@
+// import mysql from "mysql";
+
+// export const initializaDb = () => {
+//   const connection = mysql.createConnection({
+//     host: "db4free.net",
+//     user: "adusuperuser",
+//     password: "aduadmin",
+//     database: "pawsitivematchdb",
+//     port: 3306,
+//   });
+
+//   connection.connect(function (err) {
+//     if (err) {
+//       console.error("error connecting: " + err.stack);
+//       return;
+//     }
+
+//     console.log("connected as id " + connection.threadId);
+//   });
+// };
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response, Application } from "express";
+import dotenv from "dotenv";
+// import { initializaDb } from "../db";
+
+//For env File
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+app.get("/ping", (req: Request, res: Response) => {
+  res.send("pong");
+});
+
+app.get("/login", (req: Request, res: Response) => {
+  res.send("user trying to login");
+});
+
+app.listen(port, () => {
+  // initializaDb();
+  console.log(`Server is Fire at http://localhost:${port}`);
+});
