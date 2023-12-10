23c521402529efb294e8611b74468cc5893deeb8
relson01 <relsondsouza01@gmail.com>
2023-12-10 19:49:03 +0530

    added user login and signup endpoint

--- db/index.ts
+++ db/index.ts
@@ -1,20 +1,47 @@
-// import mysql from "mysql";
+import mysql from "mysql";
+// const Pool = require("pg").Pool;
 
-// export const initializaDb = () => {
-//   const connection = mysql.createConnection({
-//     host: "db4free.net",
-//     user: "adusuperuser",
-//     password: "aduadmin",
-//     database: "pawsitivematchdb",
-//     port: 3306,
-//   });
+export const initializaDb = () => {
+  const PawsitiveMatch_URL =
+    "postgres://default:4sXF1zjKuTfB@ep-small-shadow-37791384-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb";
+  const PawsitiveMatch_PRISMA_URL =
+    "postgres://default:4sXF1zjKuTfB@ep-small-shadow-37791384-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?pgbouncer=true&connect_timeout=15";
+  const PawsitiveMatch_URL_NON_POOLING =
+    "postgres://default:4sXF1zjKuTfB@ep-small-shadow-37791384.us-east-1.postgres.vercel-storage.com:5432/verceldb";
+  const PawsitiveMatch_USER = "default";
+  const PawsitiveMatch_HOST =
+    "ep-small-shadow-37791384-pooler.us-east-1.postgres.vercel-storage.com";
+  const PawsitiveMatch_PASSWORD = "4sXF1zjKuTfB";
+  const PawsitiveMatch_DATABASE = "verceldb";
+  const connection = mysql.createConnection({
+    host: "db4free.net",
+    user: "adusuperuser",
+    password: "aduadmin",
+    database: "pawsitivematchdb",
+    port: 3306,
+  });
+  // const connection = mysql.createConnection({
+  //   host: PawsitiveMatch_HOST,
+  //   user: PawsitiveMatch_USER,
+  //   password: PawsitiveMatch_PASSWORD,
+  //   database: PawsitiveMatch_DATABASE,
+  //   port: 5432,
+  // });
 
-//   connection.connect(function (err) {
-//     if (err) {
-//       console.error("error connecting: " + err.stack);
-//       return;
-//     }
+  connection.connect(function (err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
 
-//     console.log("connected as id " + connection.threadId);
-//   });
-// };
+    console.log("connected as id " + connection.threadId);
+  });
+
+  // export const pool = new Pool({
+  //   user: PawsitiveMatch_USER,
+  //   host: PawsitiveMatch_HOST,
+  //   database: PawsitiveMatch_DATABASE,
+  //   password: PawsitiveMatch_PASSWORD,
+  //   port: 5432,
+  // });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response, Application } from "express";
 import dotenv from "dotenv";
+import { initializaDb } from "../db";
+import mysql from "mysql";
+import { v4 as uuid } from "uuid";
+
 // import { initializaDb } from "../db";
 
 //For env File
@@ -7,18 +11,116 @@ dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
+app.use(express.json());
+
+const connection = mysql.createConnection({
+  host: "db4free.net",
+  user: "adusuperuser",
+  password: "aduadmin",
+  database: "pawsitivematchdb",
+  port: 3306,
+});
 
 app.get("/ping", (req: Request, res: Response) => {
   res.send("pong");
 });
 
-app.get("/login", (req: Request, res: Response) => {
-  res.send("user trying to login");
+app.post("/user/login", (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  if (!username) {
+    res.send({ status: "error", message: "username was not provided" });
+    return;
+  }
+  if (!password) {
+    res.send({ status: "error", message: "password was not provided" });
+    return;
+  }
+
+  connection.query(
+    `Select * from user where username = '${username}' and password = '${password}'`,
+    function (error, results, fields) {
+      if (error) throw error;
+      // connected!
+      // res.send(results);
+      // return;
+      console.log("login ", results);
+      if (results.length) {
+        res.send({ status: "success" });
+      } else {
+        res.send({ status: "error", message: "Wrong username or password" });
+      }
+    }
+  );
 });
+
+app.post("/user/signup", (req: Request, res: Response) => {
+  let usernameNotAvailable = false;
+  const { username, password, emailId } = req.body;
+  const uniqueId = uuid();
+
+  if (!username) {
+    res.send({ status: "error", message: "username was not provided" });
+    return;
+  }
+  if (!password) {
+    res.send({ status: "error", message: "password was not provided" });
+    return;
+  }
+  if (!emailId) {
+    res.send({ status: "error", message: "EmailID was not provided" });
+    return;
+  }
+
+  const d = connection.query(
+    `Select * from user where username = '${username}' and password = '${password}'`,
+    function (error, results, fields) {
+      if (error) throw error;
+      // connected!
+      // res.send(results);
+      // return;
+      console.log("login ", results);
+      if (results.length) {
+        usernameNotAvailable = true;
+      }
+    }
+  );
+
+  if (usernameNotAvailable) {
+    res.send({ status: "error", message: "Username Already Taken" });
+    return;
+  }
+
+  connection.query(
+    `  INSERT INTO user (id, username, password, emailId) VALUES ('${uniqueId}', '${username}', '${password}', '${emailId}');`,
+    function (error, results, fields) {
+      if (error) throw error;
+      // connected!
+      console.log("Sign up result ", results);
+      res.send({ status: "success", message: "Signed Up Successfully!" });
+    }
+  );
+});
+
+// app.get("/login", (req: Request, res: Response) => {
+//   console.log("user end started");
+//   initializaDb();
+//   // pool.
+
+//   connection.query(
+//     "  INSERT INTO user (id, username, password, emailId) VALUES ('1', 'relson2', 'password', 'email');",
+//     function (error, results, fields) {
+//       if (error) throw error;
+//       // connected!
+//     }
+//   );
+//   connection.end();
+//   res.send("user trying to login");
+// });
 
 app.listen(port, () => {
   // initializaDb();
-  console.log(`Server is Fire at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
 
 export default app;
